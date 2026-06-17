a989d4db738848e96689c5883d61a7839b33d9a7
hamza Tanveer <hamza.tanveer.dev@gmail.com>
2026-06-17 15:36:12 +0500

    Update AuthContext.js

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -7,7 +7,7 @@ export const AuthProvider = ({ children }) => {
   const [token, setToken] = useState(localStorage.getItem('token'));
   const [userId, setUserId] = useState(localStorage.getItem('userId'));
   const [loading, setLoading] = useState(false);
-const [authLoading, setAuthLoading] = useState(true);
+
  const login = useCallback((userData, userToken) => {
   setUser(userData);
   setToken(userToken);
@@ -35,7 +35,6 @@ useEffect(() => {
   const savedUserId = localStorage.getItem('userId');
   if (savedUserId) setUserId(savedUserId);
 
-  setAuthLoading(false); // 👈 IMPORTANT
 }, []);
 
   const logout = useCallback(() => {
